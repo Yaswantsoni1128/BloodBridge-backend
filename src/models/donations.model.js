d6f4052a7c9e35ef339ebf67766e1e4365df12b2
Srishti <srishtisethi31@gmail.com>
2025-08-14 11:20:28 +0530

    Create Donor Controllers

--- src/models/donations.model.js
+++ src/models/donations.model.js
@@ -1,34 +1,54 @@
-import mongoose from "mongoose"
-
-const DonationSchema = new mongoose.Schema({
-  donorId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: true
+import mongoose from "mongoose";
+import "./hospitals.model.js"
+import "./bloodRequests.model.js"
+const DonorSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      lowercase: true,
+      required: true,
+      index: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      match: [/^\S+@\S+\.\S+$/, "Please enter a valid email"],
+    },
+    phone: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      match: [/^\d{10}$/, "Enter a valid 10-digit phone number"],
+    },
+    hospitalId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Hospital",
+      required: true,
+    },
+    amount: {
+      type: Number,
+      required: true,
+    },
+    bloodType: {
+      type: String,
+      enum: ["A+", "A-", "B+", "B-", "AB+", "AB-", "O+", "O-"],
+      required: true,
+    },
+    requestId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "BloodRequest",
+    },
   },
-  hospitalId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Hospital",
-    required: true
-  },
-  amount: {
-    type: Number,
-    required: true
-  },
-  bloodType: {
-    type: String,
-    enum: ["A+", "A-", "B+", "B-", "AB+", "AB-", "O+", "O-"],
-    required: true
-  },
-  requestId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "BloodRequest",
-    required: true
+  {
+    timestamps: true,
   }
-}, {
-  timestamps: true
-});
+);
 
-const Donation = mongoose.model("Donation", DonationSchema);
+const Donor = mongoose.model("Donor", DonorSchema);
 
-export default Donation;
+export default Donor;
