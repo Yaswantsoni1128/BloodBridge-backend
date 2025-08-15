368b52eabddd3e5ede309802e8d7be92719332d2
Snehaaa-Kri <sneha001705@gmail.com>
2025-08-15 15:21:48 +0530

    added alert backend

--- src/app.js
+++ src/app.js
@@ -27,10 +27,12 @@ import smsRoutes from "./routes/sms.routes.js"
 
 import inventoryRouter from "./routes/inventory.routes.js"
 import inventorySettingsRouter from "./routes/inventorySettings.routes.js"
+import alertRoutes from './routes/alert.routes.js'
 app.use('/api/user',userRouter);
 app.use('/api/donor',donorRouter);
 app.use('/api/inventory',inventoryRouter);
 app.use('/api/inventorySettings',inventorySettingsRouter);
 app.use('/api', smsRoutes);
+app.use('/api/alert', alertRoutes);
 
 export {app}
---
+++ src/routes/alert.routes.js
@@ -0,0 +1,17 @@
+import alertController from "../controllers/alert.controller.js";
+import { validateJWT } from "../middlewares/auth.middleware.js";
+
+import express from 'express';
+const alertRouter = express.Router();
+
+//protected routes
+alertRouter.post('/', validateJWT,  alertController.createAlert);
+alertRouter.put('/:id', validateJWT,  alertController.updateAlert);
+alertRouter.delete('/:id', validateJWT,  alertController.deleteAlert);
+alertRouter.get('/hospital', validateJWT, alertController.getAllAlertsOfHospital)
+
+//public routes
+alertRouter.get('/:id',  alertController.getAlertById);
+alertRouter.get('/',  alertController.getAllAlerts);
+
+export default alertRouter;
